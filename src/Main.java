eed4e49aae9aeaed2c072c250ca1b09e14e6a710
Aleksey Zinov <AZinov@inno.tech>
2024-07-26 09:10:07 +0300

    main method for mathematical calculations has been changed

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое целое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе целое число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int sum = firstNumber + secondNumber;
+        int diff = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Сумма чисел: " + sum);
+        System.out.println("Разность чисел: " + diff);
+        System.out.println("Произведение чисел: " + product);
+        System.out.println("Частное: " + quotient);
     }
 }
