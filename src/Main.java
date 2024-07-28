2608932b5639fcbb26e2dfdcd9f7718cf04f9de9
Aleksey Zinov <AZinov@inno.tech>
2024-07-28 20:10:30 +0300

    added file availability check

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +  Math.random());
+        int count = 0;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExist = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExist && !isDirectory) {
+                System.out.println("Файл не существует или указанный путь является путём к папке, а не к файлу");
+                continue;
+            } else if (fileExist && isDirectory) {
+                count++;
+                System.out.println(count);
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + count);
+            }
+        }
     }
 }
