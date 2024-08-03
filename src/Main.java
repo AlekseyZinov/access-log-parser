251f89622f311474005fa2648061c35938ac8b2a
Aleksey Zinov <AZinov@inno.tech>
2024-08-03 22:07:31 +0300

    debag main v1.01.02

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,17 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         int count = 0;
         while (true) {
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
-            boolean fileExist = file.exists();
+            boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
-            if (!fileExist && !isDirectory) {
-                System.out.println("Файл не существует или указанный путь является путём к папке, а не к файлу");
-                continue;
-            } else if (fileExist && isDirectory) {
+            if (!fileExists || isDirectory) {
+                System.out.println("Указанный файл не существует или указанный путь является путём к папке, а не к файлу");
+            } else if (fileExists) {
                 count++;
-                System.out.println(count);
                 System.out.println("Путь указан верно");
                 System.out.println("Это файл номер " + count);
             }
